c56b77f627955d4b8c17245322d36dd7d912afdb
Shawn Pearson <1157wcl@gmail.com>
2024-11-30 10:04:53 -0500

    database tier options

--- GCP/cloudsql/main.tf
+++ GCP/cloudsql/main.tf
@@ -6,16 +6,24 @@ variable "googleProject" {
     type = string
     default="requiredbutmissing"
 }
+variable "googleRegion" {
+    type = string
+    default="requiredbutmissing"
+}
+variable "databaseTier" {
+    type = string
+    default="db-n1-standard-1"
+}
 
 resource "google_sql_database_instance" "sql_instance" {
   name             = "catalogdb-${var.uniqueIdentifier}"
   database_version = "MYSQL_8_0"
-  region           = "us-central1"
+  region           = var.googleRegion
   project = var.googleProject
 
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
-    tier = "db-n1-standard-1"
+    tier = var.databaseTier
   }
 }
