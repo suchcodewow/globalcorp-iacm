b6ae8de3ff4ebc3f9fe6f2648ccf75e73e491a9c
Shawn Pearson <1157wcl@gmail.com>
2025-05-22 12:48:20 -0400

    vm type

--- harness/gcpvm/main.tf
+++ harness/gcpvm/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "default" {
   name         = var.vm_name
-  machine_type = "n1-standard-1"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
   project      = "ecardinalhealth-459415"
   tags         = [var.vm_owner]
